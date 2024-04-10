5103956a61072d277d40442c1997ee419529344f
Kleber Coelho <klebercoelhogt@gmail.com>
2024-04-10 19:58:22 -0300

    ajustando mock.js

---
+++ mock.js
@@ -0,0 +1,40 @@
+const mockData = [
+    {
+        number: '234',
+        tipo: 'VEICULO',
+        entrada: 'R$ 12.500,00',
+        saldo: 'R$ 22.500,00',
+        parcelas: "23",
+    },
+    {
+        number: '234',
+        tipo: 'VEICULO',
+        entrada: 'R$ 12.500,00',
+        saldo: 'R$ 22.500,00',
+        parcelas: "23",
+    },
+    {
+        number: '234',
+        tipo: 'VEICULO',
+        entrada: 'R$ 12.500,00',
+        saldo: 'R$ 22.500,00',
+        parcelas: "23",
+        
+    },
+    {
+        number: '234',
+        tipo: 'VEICULO',
+        entrada: 'R$ 12.500,00',
+        saldo: 'R$ 22.500,00',
+        parcelas: "23",
+        
+    },
+    {
+        number: '234',
+        tipo: 'VEICULO',
+        entrada: 'R$ 12.500,00',
+        saldo: 'R$ 22.500,00',
+        parcelas: "23",
+        
+    }
+];
