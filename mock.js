51a6b0a81754de52d227d93a2455054051e47b48
Kleber Coelho <klebercoelhogt@gmail.com>
2024-04-20 17:14:38 -0300

    #

--- mock.js
+++ mock.js
@@ -1,40 +1,26 @@
 const mockData = [
-    {
-        number: '234',
-        tipo: 'VEICULO',
-        entrada: 'R$ 12.500,00',
-        saldo: 'R$ 22.500,00',
-        parcelas: "23",
-    },
-    {
-        number: '234',
-        tipo: 'VEICULO',
-        entrada: 'R$ 12.500,00',
-        saldo: 'R$ 22.500,00',
-        parcelas: "23",
-    },
-    {
-        number: '234',
-        tipo: 'VEICULO',
-        entrada: 'R$ 12.500,00',
-        saldo: 'R$ 22.500,00',
-        parcelas: "23",
-        
-    },
-    {
-        number: '234',
-        tipo: 'VEICULO',
-        entrada: 'R$ 12.500,00',
-        saldo: 'R$ 22.500,00',
-        parcelas: "23",
-        
-    },
-    {
-        number: '234',
-        tipo: 'VEICULO',
-        entrada: 'R$ 12.500,00',
-        saldo: 'R$ 22.500,00',
-        parcelas: "23",
-        
-    }
+  {
+    number: "234",
+    tipo: "VEICULO",
+    entrada: "R$ 12.500,00",
+    saldo: "R$ 22.500,00",
+    parcelas: "23",
+    banco: "../itau.svg",
+  },
+  {
+    number: "234",
+    tipo: "VEICULO",
+    entrada: "R$ 12.500,00",
+    saldo: "R$ 22.500,00",
+    parcelas: "23",
+    banco: "../bB.svg",
+  },
+  {
+    number: "234",
+    tipo: "VEICULO",
+    entrada: "R$ 12.500,00",
+    saldo: "R$ 22.500,00",
+    parcelas: "23",
+    banco: "../itau.svg",
+  },
 ];
